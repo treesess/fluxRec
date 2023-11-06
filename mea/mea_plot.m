17978db5cd67985637615d789052c39737456cba
treesess <shiyuhang027@gmail.com>
2023-11-07 00:11:53 +0800

    Create mea_plot.m

---
+++ mea/mea_plot.m
@@ -0,0 +1,12 @@
+
+%% 0 loading head
+meaHead211103 % mu included
+
+%% 1 plot - only one example presented here so far
+M.mk = 1;
+S = S_genOneLay_211016(T10d, T60d, M);
+plot_test_ch_percentage_211015(S)
+
+
+
+
